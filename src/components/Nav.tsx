ef10f977f44b459f1458afcbf3175b0c25dd6f13
DominikDD30 <dd301818@student.polsl.pl>
2023-12-08 14:50:12 +0100

    drugi

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { FaRegUser} from 'react-icons/fa'
 
 
 const Nav = () => {
   return (
 <nav>
     <h1>Mapa Cen</h1>
+    <div className='userPanel'>
+    <div className='userIcon'>
+      <FaRegUser size={40} color="black" />
+    </div>
+    <h2>Gosc</h2>
+    </div>
+    
 </nav>
   )
 }
