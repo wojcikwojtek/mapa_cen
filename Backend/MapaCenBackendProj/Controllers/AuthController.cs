3a8c210aa4023a4ed468f073a92e6b0f82af5e7a
michkup08 <michal1309k@gmail.com>
2023-12-19 12:30:41 +0100

    poprawa controlerow - usuniecie name

--- Backend/MapaCenBackendProj/Controllers/AuthController.cs
+++ Backend/MapaCenBackendProj/Controllers/AuthController.cs
@@ -7,9 +7,9 @@ namespace MapaCenBackend.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class LoginController
+    public class AuthController
     {
-        [HttpPost(Name = "login")]
+        [HttpPost("login")]
         public AuthResponse login([FromBody] LoginRequest loginRequest)
         {
             try
@@ -41,6 +41,33 @@ namespace MapaCenBackend.Controllers
             catch (Exception ex)
             {
                 return new AuthResponse(loginRequest.username, false);
+            }
+        }
+
+        [HttpPost("register")]
+        public void register([FromBody] RegisterDto registerDto)
+        {
+            try
+            {
+                string connstring = "server=localhost;uid=root;pwd=Mapacen;database=mapa_cen";
+                MySqlConnection conn = new MySqlConnection();
+                conn.ConnectionString = connstring;
+                conn.Open();
+                string sql = "insert into users(username, email, password, is_admin) values(@usernamearg, @emailarg, @passwordarg, 0)";
+                using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                {
+                    // Parametry dla zabezpieczenia przed atakami SQL injection
+                    cmd.Parameters.AddWithValue("@usernamearg", registerDto.getUsername());
+                    cmd.Parameters.AddWithValue("@emailarg", registerDto.getEmail());
+                    cmd.Parameters.AddWithValue("@passwordarg", registerDto.getPassword());
+
+                    // Wykonaj zapytanie
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to insert to mysql db");
             }
         }
     }
