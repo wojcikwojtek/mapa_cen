2528decc6435cd6e920712b2ec1294a852f39d6c
michkup08 <michal1309k@gmail.com>
2024-01-03 16:33:25 +0100

    dodawanie cen, dodawanie komentarzy (bez obrazka), wysyłanie najpopularniejszych produktów

---
+++ Backend/MapaCenBackendProj/DTO/AddCommentRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace MapaCenBackend.DTO
+{
+    public class AddCommentRequest
+    {
+        public int priceId { get; set; }
+        public string content { get; set; }
+
+        public AddCommentRequest(int priceId, string content)
+        {
+            this.priceId = priceId;
+            this.content = content;
+        }
+
+    }
+}
---
+++ Backend/MapaCenBackendProj/DTO/AddPriceRequest.cs
@@ -0,0 +1,20 @@
+﻿using MapaCenBackend.Entities;
+
+namespace MapaCenBackend.DTO
+{
+    public class AddPriceRequest
+    {
+        public int productId { get; set; }
+
+        public string shopAddress { get; set; }
+
+        public double priceValue { get; set; }
+
+        public AddPriceRequest( int productId, string shopAddress, double priceValue)
+        {
+            this.productId = productId;
+            this.shopAddress = shopAddress;
+            this.priceValue = priceValue;
+        }
+    }
+}
