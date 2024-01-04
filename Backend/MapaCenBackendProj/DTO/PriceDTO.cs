044d77e6032e2fa44cc7ba46cc87e559979944e9
wojcikwojtek <wojtekwojcik2310@gmail.com>
2024-01-04 17:02:33 +0100

    dodałem zliczanie łapek w górę i w dół

--- Backend/MapaCenBackendProj/DTO/PriceDTO.cs
+++ Backend/MapaCenBackendProj/DTO/PriceDTO.cs
@@ -5,6 +5,8 @@
         public string shopAddress { get; set; }
         public string date {  get; set; }
         public double priceValue { get; set; }
+        public int upvotes { get; set; }
+        public int downvotes { get; set; }
         public List<RatingDTO> ratings { get; set; }
         public List<CommentDTO> comments { get; set; }
 
@@ -15,6 +17,15 @@
             this.priceValue = priceValue;
             this.ratings = ratings;
             this.comments = comments;
+            this.upvotes = 0;
+            foreach(RatingDTO rating in ratings)
+            {
+                if(rating.isPositive == true)
+                {
+                    this.upvotes++;
+                }
+            }
+            this.downvotes = ratings.Count - this.upvotes;
         }
     }
 }
--- Backend/MapaCenBackendProj/obj/Debug/net8.0/MapaCenBackend.AssemblyInfo.cs
+++ Backend/MapaCenBackendProj/obj/Debug/net8.0/MapaCenBackend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MapaCenBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+662de3cd3a8e165897934f0cb937c578c4f1491c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+954e1348759c86d15b2df62a4e7fd9194ac2d4fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapaCenBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapaCenBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
