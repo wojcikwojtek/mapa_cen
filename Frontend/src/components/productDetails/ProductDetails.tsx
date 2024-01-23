07ce1c7ae119111d884ac2168be78119ad5dcd5c
DominikDD30 <dd301818@student.polsl.pl>
2024-01-23 15:30:49 +0100

    nowe

--- Frontend/src/components/productDetails/ProductDetails.tsx
+++ Frontend/src/components/productDetails/ProductDetails.tsx
@@ -71,10 +71,7 @@ const ProductDetails = () => {
       {productPrices.data?.length !=0 ?
       <>
       {productPrices.data?.map(price=>
-      {
-      console.log(price);
-      <ProductItem key={price.priceId} priceInfo={price} updateComponent={()=>{productPrices.refetch();}}/>
-      }
+       <ProductItem key={price.priceId} priceInfo={price} updateComponent={()=>{productPrices.refetch();}}/>
       )}
       </>
       :
