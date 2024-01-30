5e64ec4bf329a33c4731031cd4850854d76e8d1f
DominikDD30 <dd301818@student.polsl.pl>
2024-01-30 15:34:20 +0100

    dodawanie przez admina produtku no i dodawanie ceny w trakcie

---
+++ Frontend/src/components/productDetails/AddPriceForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddPriceForm = () => {
+  return (
+    <div className='addPriceForm'>AddPriceForm</div>
+  )
+}
+
+export default AddPriceForm
--- Frontend/src/components/productDetails/ProductDetails.tsx
+++ Frontend/src/components/productDetails/ProductDetails.tsx
@@ -1,41 +1,14 @@
 import { useParams } from 'react-router-dom';
 import useProductDetails from '../../hooks/useProductDetails';
 import { FaCrown } from "react-icons/fa6";
-import { useState } from 'react';
-import ProductItem from './ProductItem';
-import useUserStore from '../../store';
-import useProvinces from '../../hooks/useProvinces';
-import usePowiaty from '../../hooks/usePowiaty';
-import useProductPrices from '../../hooks/useProductPrices';
-import React from 'react';
+import ProductPrices from './productPrices';
 
 
 const ProductDetails = () => {
     const {productId}=useParams();
-    const userStore=useUserStore();
     const {data}=useProductDetails(parseInt(productId!));
-    const [selectedPowiat,setSelectedPowiat]=useState(0);
     // const [selectedProvinceIndex,setSelectedProvinceIndex]=useState(0);
-    const provinceData =useProvinces();
-    const [selectedProvince,setSelectedProvince]=useState(provinceData.data?.
-      find(province=>province.id==userStore.userRegion)?.name||'');
-      const [powiaty,setPowiaty]=useState(userStore.userRegion);
-    const powiatData =usePowiaty(powiaty);
-
-    
-    
-    const productPrices =useProductPrices(parseInt(productId!),selectedPowiat);
   
-
-    const handleBack=()=>{
-      if(selectedPowiat!=0){
-        setSelectedPowiat(0);
-      }
-      else{
-        setSelectedProvince('');
-      }
-    }
-   
  
   return (
     <main>
@@ -54,55 +27,7 @@ const ProductDetails = () => {
     <div className='mapWrapper'>
       <h2 style={{width:'100%',textAlign:'center'}} >Mapa albo Informacje o konkretnej cenie</h2>
     </div>
-    <div className='productPrices'>
-      {selectedProvince?
-      <>
-      <div className='singleProvince'>
-         <span>{selectedProvince}  {selectedPowiat? 
-         `powiat ${powiatData.data?.find(powiat=>powiat.id==selectedPowiat)?.name}`:''}
-         </span>
-        <button onClick={handleBack}>
-          powrót
-        </button>
-      </div>
-      
-      {selectedPowiat ?
-      <div className='priceItems'>
-      {productPrices.data?.length !=0 ?
-      <>
-      {productPrices.data?.map(price=>
-       <ProductItem key={price.priceId} regionId={selectedPowiat} priceInfo={price} updateComponent={()=>{productPrices.refetch();}}/>
-      )}
-      </>
-      :
-      <h4 style={{marginTop:'20px'}}>Ten Produkt nie ma aktualnie żadnych wpisów w tym Powiecie</h4>
-      }
-      </div>
-      :
-      <div>
-      {powiatData.data?.map(powiat=>
-      <div className='listItem' key={powiat.id} style={{cursor:'pointer'}} onClick={()=>setSelectedPowiat(powiat.id)}>
-      {powiat.name}
-      </div>
-      )}
-      </div>
-      }
-
-       
-
-      </>
-      :
-      <>
-      <h3>Wybierz województwo</h3>
-      {provinceData.data?.map((province,index) => (
-          <div className='province' key={index}
-           onClick={()=>{setSelectedProvince(province.name);setPowiaty(province.id);}}>
-        {province.name}
-          </div>
-        ))}
-      </>
-      }
-      </div>
+    <ProductPrices productId={productId!}/>
     </section>
     </main>
   )
